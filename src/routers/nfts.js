a714f6d226ac0b16a2e597ad2cfaa7c77fab4e7e
Huy <humghuy201280@gmail.com>
2022-03-03 13:23:04 +0700

    update - try/catch

--- src/routers/nfts.js
+++ src/routers/nfts.js
@@ -7,25 +7,28 @@ const { response } = require("express");
 const pinJSONToIPFS = require("../middleware/pinata");
 
 const PUBLIC_KEY = "0x80e7e19d5950121304a2a4D265582a05cF2099f3";
-const PRIVATE_KEY = "da2117b83749a891fc6017bdaa34f2fd6a8d0581a335921ae80e40e95aa4e1b6";
+const PRIVATE_KEY =
+  "da2117b83749a891fc6017bdaa34f2fd6a8d0581a335921ae80e40e95aa4e1b6";
 var Web3 = require("web3");
-var web3 = new Web3("https://speedy-nodes-nyc.moralis.io/12c36cfbdd209707bb91d9a7/bsc/testnet");
+var web3 = new Web3(
+  "https://speedy-nodes-nyc.moralis.io/12c36cfbdd209707bb91d9a7/bsc/testnet"
+);
 const tokenaddress = "0x535a939aC42F70954FFd8204d384f56b032ddE0f";
 const Token = require("../../Token.json");
 const tokenContract = new web3.eth.Contract(Token.abi, tokenaddress);
 
 //withdraw || mint nft
 const mintToken = async (recipdent, tokenURI) => {
-  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
   //the transaction
   const tx = {
-    'from': PUBLIC_KEY,
-    'to': tokenaddress,
-    'nonce': nonce,
-    'gas': 500000,
-    'data': tokenContract.methods.mintNFT(recipdent, tokenURI).encodeABI()
+    from: PUBLIC_KEY,
+    to: tokenaddress,
+    nonce: nonce,
+    gas: 500000,
+    data: tokenContract.methods.mintNFT(recipdent, tokenURI).encodeABI(),
   };
-  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
   signPromise
     .then((signedTx) => {
       web3.eth.sendSignedTransaction(
@@ -34,22 +37,22 @@ const mintToken = async (recipdent, tokenURI) => {
           if (!err) {
             console.log(
               "The hash of your transaction is: ",
-              hash,
+              hash
               // "\nCheck Alchemy's Mempool to view the status of your transaction!"
-            )
+            );
           } else {
             console.log(
               "Something went wrong when submitting your transaction:",
               err
-            )
+            );
           }
         }
-      )
+      );
     })
     .catch((err) => {
-      console.log(" Promise failed:", err)
-    })
-}
+      console.log(" Promise failed:", err);
+    });
+};
 
 router.get("/", async function (req, res) {
   //const data = await getToken();
@@ -59,29 +62,36 @@ router.get("/", async function (req, res) {
 });
 
 router.get("/:id", async function (req, res) {
-  const id = parseInt(req.params.id);
-  //const data = await getToken();
-  const result = await pool.query(
-    'SELECT * FROM nft WHERE id = $1',
-    [id]
-  );
-  console.log("name: ", result.rows[0].name);
-  res.json(result.rows);
+  try {
+    const id = parseInt(req.params.id);
+    //const data = await getToken();
+    const result = await pool.query("SELECT * FROM nft WHERE id = $1", [id]);
+    if (result.rows.length == 0) {
+      throw `No NFT found with id ${id}`;
+    }
+    res.json(result.rows);
+  } catch (e) {
+    res.status(404).send({
+      error: e.toString(),
+    });
+  }
 });
 
 router.post("/", async (req, res) => {
   const { id, name, image, description, addressOwner } = req.body;
   try {
-    console.log("run");
     const result = await pool.query(
       `INSERT INTO nft (id,name,image,description,addressowner) 
-      VALUES (${id},'${name}','${image}','${description}','${addressOwner}')
+      VALUES ($1,$2,$3,$4,$5)
       RETURNING id,name,image,description,addressowner;
-      `
+      `,
+      [id, name, image, description, addressOwner]
     );
     res.send(result.rows);
   } catch (e) {
-    res.status(500).send();
+    res.status(500).send({
+      error: e.toString(),
+    });
     console.log(e);
   }
 });
@@ -90,11 +100,11 @@ router.put("/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const { addressOwner } = req.body;
   try {
-    const result = await pool.query(`UPDATE nft Set addressowner = $1 WHERE id = $2`,
+    const result = await pool.query(
+      `UPDATE nft Set addressowner = $1 WHERE id = $2`,
       [addressOwner, id]
     );
-    res.send(result.rows)
-    res.status(200).send(`User modified with ID: ${id}`)
+    res.status(200).send(`User modified with ID: ${id}`);
   } catch (e) {
     res.status(500).send();
     console.log(e);
@@ -106,10 +116,7 @@ router.delete("/:id", async function (req, res) {
   const id = parseInt(req.params.id);
   const { addressWithdraw } = req.body;
   //get data of nft
-  let result = await pool.query(
-    'SELECT * FROM nft WHERE id = $1',
-    [id]
-  );
+  let result = await pool.query("SELECT * FROM nft WHERE id = $1", [id]);
   //make metadata
   const metadata = new Object();
   metadata.name = result.rows[0].name;
@@ -118,27 +125,25 @@ router.delete("/:id", async function (req, res) {
 
   try {
     //make pinata call
-    const pinataResponse = await pinJSONToIPFS(metadata)
+    const pinataResponse = await pinJSONToIPFS(metadata);
     if (!pinataResponse.success) {
       return {
         success: false,
         status: "😢 Something went wrong while uploading your tokenURI.",
-      }
+      };
     }
-    const tokenURI = pinataResponse.pinataUrl
-    console.log("TokenURI: ", tokenURI)
+    const tokenURI = pinataResponse.pinataUrl;
+    console.log("TokenURI: ", tokenURI);
 
     //withdraw nft || mint
-    mintToken(addressWithdraw, tokenURI)
+    mintToken(addressWithdraw, tokenURI);
   } catch (error) {
-    console.log('Error uploading file: ', error)
+    console.log("Error uploading file: ", error);
   }
-  
+
   //delete data in database
-  result = await pool.query(
-    'DELETE FROM nft where id = $1', [id]
-  );
-  res.status(200).send(`User deleted with ID: ${id}`)
+  result = await pool.query("DELETE FROM nft where id = $1", [id]);
+  res.status(200).send(`User deleted with ID: ${id}`);
 });
 
 module.exports = router;
